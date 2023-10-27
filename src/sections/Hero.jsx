7fbe9197733cb2c7b454b5a8839e83747380bc95
Md Zafer Eqbal <mdzafereqbal@gmail.com>
2023-10-27 18:56:16 +0530

    Text-update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+        <p className="text-xl font-montserrat text-coral-red"><span className="font-bold">This Website is under Development</span><br/>Our Summer Collection</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10 ">
             The New Arrival
